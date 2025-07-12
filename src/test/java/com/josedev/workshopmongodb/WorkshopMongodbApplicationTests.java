6db75083c01f75a715ca95bb00adf8d67d553917
Jose Freires <joseguifreires@gmail.com>
2025-07-12 13:57:34 -0300

    First commit - Created project

---
+++ src/test/java/com/josedev/workshopmongodb/WorkshopMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josedev.workshopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
